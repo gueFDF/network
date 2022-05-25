a5d1d0bcdb3e937b5457a679009bca6167987f20
郭天宇 <3237455241@qq.com>
2022-05-25 21:10:06 +0800

    客户端2

---
+++ scoket编程/client.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<errno.h>
+#include<stdlib.h>
+#define SERV_PORT 9527
+void sys_err(const char*str)
+{
+    perror(str);
+    exit(1);
+}
+int main()
+{
+    char buf[BUFSIZ];
+    int cfd=0;
+    cfd=socket(AF_INET,SOCK_STREAM,0);
+    struct sockaddr_in servaddr;
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_port=htons(SERV_PORT);
+    inet_pton(AF_INET,"127.0.0.1",&servaddr.sin_addr.s_addr);
+    connect(cfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+    while(1)
+    {
+        write(cfd,"hello",5);
+        int ret=read(cfd,buf,sizeof(buf));
+        sleep(1);
+        write(1,buf,ret);
+        printf("\n");
+    }
+    close(cfd);
+    return 0;
+}
