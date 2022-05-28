db172736019987ad7e8b39b44277bfff5c4157e2
郭天宇 <3237455241@qq.com>
2022-05-28 15:43:52 +0800

    封装的Readn Writen Readline

--- scoket编程/HEAD.h
+++ scoket编程/HEAD.h
@@ -26,4 +26,53 @@ ssize_t Readn(int fd,void*vptr,size_t n)
     }
     return n-nleft;
 }
-static ssize_t my_read
+ssize_t Writen(int fd,const void *vptr,size_t n)
+{
+    size_t nleft;
+    ssize_t nwritten;
+    const char*ptr;
+    ptr=(char*)vptr;
+    nleft=n;
+    while(nleft>0)
+    {
+        if((nwritten=write(fd,ptr,nleft))<=0)
+        {
+            if(nwritten<0&&errno==EINTR)
+            nwritten=0;
+            else 
+            return -1;
+        }
+        nleft-=nwritten;
+        ptr+=nwritten;
+    }
+    return n;
+}
+ssize_t Readline(int fd,void*vptr,size_t maxlen)
+{
+    ssize_t n,rc;
+    char c,*ptr;
+    ptr=(char*)vptr;
+    for(n=1;n<maxlen;n++)
+    {
+        again:
+        if((rc=read(fd,&c,1))==1)
+        {
+            *ptr++=c;
+            if(c=='\n')
+            break;
+        }
+        else if(rc==0)
+        {
+            *ptr=0;
+            return (n-1);
+        }
+        else
+        {
+            if(errno==EINTR)
+            goto again;
+              return(-1);
+        }
+    }
+    *ptr=0;
+    return n;
+}
