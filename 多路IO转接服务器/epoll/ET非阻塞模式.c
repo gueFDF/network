ba28eb33a1ddd3931d3b333e9a8e91269e65773e
郭天宇 <3237455241@qq.com>
2022-06-08 10:57:15 +0800

    修改bug

--- 多路IO转接服务器/epoll/ET非阻塞模式.c
+++ 多路IO转接服务器/epoll/ET非阻塞模式.c
@@ -4,12 +4,13 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
-#include<fcntl.h>
+#include <fcntl.h>
 #include <ctype.h>
-#include"HEAD.h"
+#include "HEAD.h"
 #include <sys/epoll.h>
-#define SERV_PORT 6666
+#define SERV_PORT 6665
 #define OPEN_MAX 1024
+
 #define EPOLLLEN 1024
 #define MAXLEN 10
 void sys_error(const void *str)
@@ -19,48 +20,49 @@ void sys_error(const void *str)
 }
 int main()
 {
-    int efd,lfd,cfd;
-    int buf[BUFSIZ];
-    int i,j;
+    int efd, lfd, cfd;
+    char buf[BUFSIZ];
+    int i, j;
     socklen_t len;
-    int ret;//接受返回值
-    struct sockaddr_in sev_addr,clt_addr;
-    struct epoll_event temp,events[10];
-    lfd=socket(AF_INET,SOCK_STREAM,0);
+    int ret; //接受返回值
+    struct sockaddr_in sev_addr, clt_addr;
+    struct epoll_event temp, events[10];
+    lfd = socket(AF_INET, SOCK_STREAM, 0);
     int opt;
-    setsockopt(lfd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(int));//允许端口复用
-    if(lfd==-1)
-    sys_error("socket error");
-    bzero(&sev_addr,sizeof(sev_addr));
-    sev_addr.sin_family=AF_INET;
-    sev_addr.sin_port=htons(SERV_PORT);
-    sev_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-    ret=bind(lfd,(struct sockaddr*)&sev_addr,sizeof(sev_addr));
-    if(ret==-1)
-    sys_error("bind error");
-    ret=listen(lfd,128);
-    if(ret==-1)
-    sys_error("listen error");
-    efd=epoll_create(10);;//创建监听红黑树
-    temp.events=EPOLLIN|EPOLLET;
-    len=sizeof(clt_addr);
-    cfd=accept(lfd,(struct sockaddr*)&clt_addr,&len);
+    setsockopt(lfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int)); //允许端口复用
+    if (lfd == -1)
+        sys_error("socket error");
+    bzero(&sev_addr, sizeof(sev_addr));
+    sev_addr.sin_family = AF_INET;
+    sev_addr.sin_port = htons(SERV_PORT);
+    sev_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    ret = bind(lfd, (struct sockaddr *)&sev_addr, sizeof(sev_addr));
+    if (ret == -1)
+        sys_error("bind error");
+    ret = listen(lfd, 128);
+    if (ret == -1)
+        sys_error("listen error");
+    efd = epoll_create(10);          //创建监听红黑树
+    temp.events = EPOLLIN | EPOLLET; // ET模式
+    len = sizeof(clt_addr);
+    cfd = accept(lfd, (struct sockaddr *)&clt_addr, &len);
     printf("accept success\n");
-    temp.data.fd=cfd;
-    epoll_ctl(efd,EPOLL_CTL_ADD,cfd,&temp);
+    temp.data.fd = cfd;
+    epoll_ctl(efd, EPOLL_CTL_ADD, cfd, &temp);
     //设置非阻塞
     int flag;
-    flag=fcntl(cfd,F_GETFL);
-    flag|=O_NONBLOCK;
-    fcntl(cfd,F_SETFL,flag);
-    while(1)
+    flag = fcntl(cfd, F_GETFL);
+    flag |= O_NONBLOCK;
+    fcntl(cfd, F_SETFL, flag); //设置非阻塞
+    while (1)
     {
-        ret=epoll_wait(efd,events,10,-1);
-        if(events[0].data.fd==cfd)
+        int len1;
+        ret = epoll_wait(efd, events, 10, -1);
+        if (events[0].data.fd == cfd)
         {
-            if((len=read(cfd,buf,MAXLEN/2))>0)
+            while ((len1 = read(cfd, buf, MAXLEN / 2)) > 0)
             {
-                write(1,buf,len);
+                write(1, buf, len1);
             }
         }
     }
