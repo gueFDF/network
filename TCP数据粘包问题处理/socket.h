139afcd845c2567cf79db51756f5f6c4bc206a9a
gueFDF <3237455241@qq.com>
2022-06-14 15:36:54 +0800

    函数声明

--- TCP数据粘包问题处理/socket.h
+++ TCP数据粘包问题处理/socket.h
@@ -1,15 +1,28 @@
 #ifndef _SOCKET_H
 #define _SOCKET_H 
+#include<stdio.h>
+#include<sys/socket.h>
+#include<arpa/inet.h> 
+#include<unistd.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<strings.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/epoll.h>
 //绑定+监听
 int setListen(int lfd,unsigned short port);
-int acceptConn(int lfd,struct socket_in*addr);
+int acceptConn(int lfd,struct sockaddr_in*addr);
 
 //客户端
-int connectToHost(int fd,const char*ip,unsigned short port);
+int connectToHost(int lfd, char*ip,unsigned short port);
 
 
-int creatsocket();
+int createsocket();
 int sendMsg(int fd,const char*msg,int len);
 int recvMsg(int fd,char**msg);
+int writen(int fd, char*msg,int size);
+int readn(int fd,char*buf,int size);
 
 #endif
