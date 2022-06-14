1430e59402022a7e38a32787fad4d65c8cd76558
gueFDF <3237455241@qq.com>
2022-06-15 01:21:48 +0800

    客户端

--- TCP数据粘包问题处理/client.c
+++ TCP数据粘包问题处理/client.c
@@ -1,21 +1,23 @@
 #include"socket.h"
 int main()
 {
-  char temp[1000];
-  int fd=createsocket();
+  char temp[1001];
+  int fd=socket(AF_INET,SOCK_STREAM,0);
+  struct sockaddr_in caddr;
+  caddr.sin_family=AF_INET;
+  caddr.sin_port=htons(9526);
+  inet_pton(AF_INET,"127.0.0.1",&caddr.sin_addr.s_addr);
+  connect(fd,(struct sockaddr*)&caddr,sizeof(caddr));
+  printf("客户端连接成功\n");
   if(fd==-1)
     return -1;
-  int ret=connectToHost(fd,"127.0.0.1",9527);
-  if(ret==-1)
-  {
-    return -1;
-  }
+  
   int length=0;
   int fd1=open("english.txt",O_RDONLY);
   while((length=read(fd1,temp,rand()%1000))>0)
   {
     sendMsg(fd,temp,length);
-    memset(temp,0,sizeof(temp));;
+    memset(temp,0,sizeof(temp));
     usleep(300);
   }
   sleep(10);
