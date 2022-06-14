40e55d009ee6e436aaa1276a47b88efe68f2bdfc
gueFDF <3237455241@qq.com>
2022-06-15 01:22:39 +0800

    服务器

--- TCP数据粘包问题处理/serve.c
+++ TCP数据粘包问题处理/serve.c
@@ -3,14 +3,23 @@
 int main()
 {
   int i;
-  int ret;//检测返回值
+  int ret=-1;//检测返回值
   int cfd;
   char *buf;
-  int fd=createsocket();
-  if(fd==-1)
+  int listenfd=createsocket();
+  if(listenfd==-1)
     return -1;
- int listenfd=0;
- listenfd=setListen(fd,9527);
+ struct sockaddr_in saddr;
+ saddr.sin_family=AF_INET;
+ saddr.sin_port=htons(9526);
+ saddr.sin_addr.s_addr=INADDR_ANY;
+ ret=bind(listenfd,(struct sockaddr*)&saddr,sizeof(saddr));
+ if(ret==-1)
+ {
+   perror("bind error");
+   exit(-1);
+ }
+ ret=listen(listenfd,128);
  int epfd=epoll_create(MAXLEN);  //创建监听红黑树
  struct epoll_event temp,ep[1024];
  temp.data.fd=listenfd;
@@ -18,6 +27,7 @@ int main()
  epoll_ctl(epfd,EPOLL_CTL_ADD,listenfd,&temp);//将监听套接字加入监听红黑树
 while(1)
 {
+
   ret=epoll_wait(epfd,ep,1024,-1);
   for(i=0;i<ret;i++)
   {
@@ -27,12 +37,13 @@ while(1)
       temp.data.fd=cfd;
       temp.events=EPOLLIN;
       epoll_ctl(epfd,EPOLL_CTL_ADD,cfd,&temp);
+      printf("客户端%d连接成功\n",cfd);
     }
     else 
     {
       int ccfd=ep[i].data.fd;
       int len=recvMsg(ccfd,&buf);
-      if(len==0)
+     if(len==0)
       {
         temp.data.fd=ccfd;
         temp.events=EPOLLIN;
@@ -41,14 +52,14 @@ while(1)
       }
       else 
       {
-        printf("%s",buf);
+        printf("%s\n\n\n\n",buf);
         free(buf);
       }
+      sleep(1);
     }
   }
 }
 
  close(listenfd);
-
   return 0;
 }
