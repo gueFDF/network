4f9a8510f2d82768ef406a3d3dc26bb9b5bf1640
gueFDF <3237455241@qq.com>
2022-10-17 18:04:25 +0800

    add 2 trests

--- scoket编程/client.c
+++ scoket编程/client.c
@@ -14,12 +14,19 @@ int main()
 {
     char buf[BUFSIZ];
     int cfd=0;
+    int ret;
     cfd=socket(AF_INET,SOCK_STREAM,0);
     struct sockaddr_in servaddr;
     servaddr.sin_family=AF_INET;
     servaddr.sin_port=htons(SERV_PORT);
     inet_pton(AF_INET,"127.0.0.1",&servaddr.sin_addr.s_addr);
-    connect(cfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+    ret=connect(cfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+    if(ret==-1)
+    {
+        perror("connect error");
+        exit(1);
+    }
+    printf("连接成功\n");
     while(1)
     {
         write(cfd,"hello",5);
