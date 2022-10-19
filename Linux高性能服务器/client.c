c4a140a2319bb9a489dba811753f0f86feca3793
gueFDF <3237455241@qq.com>
2022-10-19 09:40:57 +0800

    两个客户端互相connect

--- Linux高性能服务器/client.c
+++ Linux高性能服务器/client.c
@@ -20,28 +20,54 @@ int main(int argc, char *argv[])
     address.sin_family = AF_INET;
     address.sin_port = htons(port);
     inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
-    int ret = connect(cfd, (struct sockaddr *)&address, sizeof(address));
-    if (ret == -1) //连接失败
+    bind(cfd, (struct sockaddr *)&address, sizeof(address));
+
+    struct sockaddr_in copy;
+    memcpy(&copy, &address, sizeof(address));
+    int ret;
+    copy.sin_port = htons(9999);
+    inet_pton(AF_INET, "192.168.30.139", &copy.sin_addr.s_addr);
+    while (1)
     {
-        perror("connect fale");
-        exit(1);
+        ret = connect(cfd, (struct sockaddr *)&copy, sizeof(address));
+        if (ret == 0) //连接成功
+        {
+            printf("conne success");
+            break;
+        }
+         printf("connfd = %d errno = %d\n", ret, errno);
+         perror("connect");
     }
-    else //连接成功
+    //连接成功
     {
         while (1)
         {
-            printf("sdsdf\n");
-            ret = read(STDIN_FILENO, buf, sizeof(buf));
-            printf("ret = %d\n", ret);
-            ret = write(cfd, buf, ret);
-            printf("ret = %d\n", ret);
-            // write(cfd,buf,)
-            ret = read(cfd, buf, sizeof(buf));
-            printf("ret = %d\n", ret);
-            if (ret <= 0)
+
+            // ret = read(STDIN_FILENO, buf, sizeof(buf));
+            // printf("ret = %d\n", ret);
+            // ret = write(cfd, buf, ret);
+            // printf("ret = %d\n", ret);
+            // // write(cfd,buf,)
+            // ret = read(cfd, buf, sizeof(buf));
+            // printf("ret = %d\n", ret);
+            // if (ret <= 0)
+            // {
+            //     close(cfd);
+            //     return 0;
+            // }
+
+            int pid = fork();
+            if (pid == 0) //父进程
             {
-                close(cfd);
-                return 0;
+                //进行写
+                ret = read(STDIN_FILENO, buf, sizeof(buf));
+                ret = write(cfd, buf, ret);
+            }
+            else
+            {
+                //进行读
+                ret = read(cfd, buf, sizeof(buf));
+                write(STDOUT_FILENO, buf, ret);
             }
         }
     }
--- Linux高性能服务器/test6-4.c
+++ Linux高性能服务器/test6-4.c
@@ -25,43 +25,87 @@ int main(int argc, char *argv[])
     address.sin_port = htons(port);
     int sock = socket(PF_INET, SOCK_STREAM, 0);
     assert(sock >= 0);
+    int opt = 1;
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
     int ret = bind(sock, (struct sockaddr *)&address, sizeof(address));
     assert(ret != -1);
-    ret = listen(sock, 5);
+    //  ret = listen(sock, 5);
     assert(ret != -1);
     struct sockaddr_in client;
     socklen_t client_addrlength = sizeof(client);
-    int connfd = accept(sock, (struct sockaddr *)&client, &client_addrlength);
-    if (connfd < 0)
+    // int connfd = accept(sock, (struct sockaddr *)&client, &client_addrlength);
+
+    struct sockaddr_in copy;
+    memcpy(&copy, &address, sizeof(address));
+
+    copy.sin_port = htons(10000);
+
+    while (1)
     {
-        printf("errno is:%d\n", errno);
-    }
-    else
-    {
-        int pipefd[2];
-        assert(ret != -1);
-        ret = pipe(pipefd); /*创建管道*/
-        while (1)
+        int connfd = connect(sock, (struct sockaddr *)&copy, sizeof(address));
+        if (connfd == 0)
         {
-            /*将connfd上流入的客户数据定向到管道中*/
-            ret = splice(connfd, NULL, pipefd[1], NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
-            if (ret <= 0)
-            {
-                printf("客户端关闭\n");
-                goto flag;
-            }
-            printf("ret : %d\n", ret);
-            /*将管道的输出定向到connfd客户连接文件描述符*/
-            ret = splice(pipefd[0], NULL, connfd, NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
-            if (ret <= 0)
-            {
-                printf("客户端关闭\n");
-                goto flag;
-            }
+            printf("conne success\n");
+            break;
+        }
+        // printf("connectfd = %d\n", connfd);
+        // printf("connfd = %d errno = %d\n", connfd, errno);
+        // perror("connect");
+    }
+
+    // int pipefd[2];
+    // assert(ret != -1);
+    // ret = pipe(pipefd); /*创建管道*/
+    // while (1)
+    // {
+    //     /*将connfd上流入的客户数据定向到管道中*/
+    //     ret = splice(connfd, NULL, pipefd[1], NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
+    //     if (ret <= 0)
+    //     {
+    //         printf("客户端关闭\n");
+    //         goto flag;
+    //     }
+    //     printf("ret : %d\n", ret);
+    //     /*将管道的输出定向到connfd客户连接文件描述符*/
+    //     ret = splice(pipefd[0], NULL, connfd, NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
+    //     if (ret <= 0)
+    //     {
+    //         printf("客户端关闭\n");
+    //         goto flag;
+    //     }
+    // }
+    char buf[1024];
+    while (1)
+    {
+        // ret = read(STDIN_FILENO, buf, sizeof(buf));
+        // printf("ret = %d\n", ret);
+        // ret = write(sock, buf, ret);
+        // printf("ret = %d\n", ret);
+        // // write(cfd,buf,)
+        // ret = read(sock, buf, sizeof(buf));
+        // printf("ret = %d\n", ret);
+        // if (ret <= 0)
+        // {
+        //     close(sock);
+        //     return 0;
+        // }
+
+        int pid = fork();
+        if (pid == 0) //父进程
+        {
+            //进行写
+            ret = read(STDIN_FILENO, buf, sizeof(buf));
+            ret = write(sock, buf, ret);
+        }
+        else
+        {
+            //进行读
+            ret = read(sock, buf, sizeof(buf));
+            write(STDOUT_FILENO, buf, ret);
         }
     }
+
 flag:
-    close(connfd);
     close(sock);
     return 0;
 }
